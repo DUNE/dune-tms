ee2ff3b2eaa70263cedd9beda56ff6e0267b021f
Clarence Wret <cwret@fnal.gov>
2021-07-21 17:00:17 -0500

    remove verbosity

--- app/ConvertToTMSTree.cpp
+++ app/ConvertToTMSTree.cpp
@@ -54,7 +54,7 @@ bool ConvertToTMSTree(std::string filename, std::string output_filename) {
   TMS_Geom::GetInstance().SetGeometry(geom);
 
   int N_entries = events->GetEntries();
-  N_entries = 110;
+  N_entries = 1100;
 
   std::cout << "Starting loop over " << N_entries << " entries..." << std::endl;
   TStopwatch Timer;
@@ -66,11 +66,10 @@ bool ConvertToTMSTree(std::string filename, std::string output_filename) {
     gRoo->GetEntry(i);
 
 #ifndef DEBUG
-    //if (i % (N_entries/10) == 0) {
+    if (i % (N_entries/10) == 0) {
 #endif
       std::cout << "Processed " << i << "/" << N_entries << " (" << double(i)*100./N_entries << "%)" << std::endl;
-      //tree->AutoSave();
-    //}
+    }
 
     // Make a TMS event
     TMS_Event tms_event = TMS_Event(*event);
