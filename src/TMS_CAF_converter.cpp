45c98fb3d9e7c0fff2a52a160ab2477531bb5c71
Clarence Wret <cwret@fnal.gov>
2022-02-11 21:43:22 -0600

    Update CAF maker to include hough line information

--- src/TMS_CAF_converter.cpp
+++ src/TMS_CAF_converter.cpp
@@ -1,7 +1,9 @@
+#include "TMS_CAF_converter.h"
+
 void TMS_CAF_converter::TMS_CAF_converter() {
 }
 
-void TMS_CAF_converter::ConvertEvent(TMS_event &event) {
+caf::SRTMS TMS_CAF_converter::ConvertEvent(TMS_event &event) {
   std::vector<std::vector<TMS_Hit> > LineCandidates = TMS_TrackFinder::GetFinder().GetHoughCandidates();
 
   // CAF object for TMS
@@ -14,6 +16,7 @@ void TMS_CAF_converter::ConvertEvent(TMS_event &event) {
   for (auto it = LineCandidates.begin(); it != LineCandidates.end(); ++it) {
     // Get the track
     auto track = *it;
+
     // caf tracks are pretty simple right now; 
     // only start point, end point, start, direction and end direction
     TMS_Hit firsthit = track.first();
@@ -22,7 +25,33 @@ void TMS_CAF_converter::ConvertEvent(TMS_event &event) {
     SRVector3 firsthit_conv(firsthit.GetNotZ(), 200, firsthit.GetZ()); // Have no y info in the TMS
     SRVector3 lasthit_conv(lasthit.GetNotZ(), 200, lasthit.GetZ()); // Have no y info in the TMS
 
-    tracks.start = firsthit_conv;
-    tracks.end = lasthit_conv;
+    caf::SRTrack track;
+    track.start = firsthit_conv;
+    track.end = lasthit_conv;
+    tracks.push_back(track);
   }
+
+  std::vector<std::pair<bool, TF1> > HoughLines = TMS_TrackFinder::GetFinder().GetHoughLines();
+  int nit = 0;
+  for (auto it = HoughLines.begin(); it != HoughLines.end(); ++it, ++nit) {
+    double intercept = (*it).second->GetParameter(0);
+    double slope = (*it).second->GetParameter(1);
+
+    // Calculate the z and x vectors by evaling the TF1 in thin and thick target
+    double zlow = TMS_Const::TMS_Thin_Start;
+    double zhi = TMS_Const::TMS_Thick_Start;
+    double xlow = Lines.second->Eval(zlow);
+    double xhi = Lines.second->Eval(zhi);
+
+    double zlen = zhi-zlow;
+    double xlen = xhi-xlow;
+    double len = sqrt(xlen*xlen+zlen*zlen);
+    zlen = zlen/len;
+    xlen = xlen/len;
+
+    SRVector3D dir(xlen, 0, zlen); // Make the converted direction unit vector
+    tracks[nit].dir = dir;
+  }
+
+  return caf;
 }
--- src/TMS_CAF_converter.h
+++ src/TMS_CAF_converter.h
@@ -2,18 +2,22 @@
 #define __TMS_CAF_CONVERTER__
 
 // Can only run this if we have linkage to CAF format
-#ifdef __CAF_LINKAGE__
+#ifdef __DUNEANAOBJ_ENABLED__
 
 #include <iostream>
 
-// Include CAF
-#include
+// Include the general TMS_Event class
+#include "TMS_Event.h"
+
+// Include standard record format for TMS
+#include "SRTMS.h"
 
 // Converter for TMS format to CAF format
 class TMS_CAF_converter {
   public:
     TMS_CAF_converter();
-    ConvertEvent();
+    ~TMS_CAF_converter() {};
+    caf::SRTMS ConvertEvent(TMS_event &event);
   private:
 }
 
