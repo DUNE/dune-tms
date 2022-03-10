d82f2e23ec546a50ddfd592031a568792c9e47d8
Clarence Wret <cwret@fnal.gov>
2022-03-10 13:56:32 -0600

    Add reconstructed hits to event display

--- app/DrawEvents.cpp
+++ app/DrawEvents.cpp
@@ -19,6 +19,7 @@
 
 #include "TMS_Event.h"
 #include "TMS_Constants.h"
+#include "TMS_Reco.h" // Needed for hit cleaning
 
 int main(int argc, char** argv) {
   if (argc != 2) {
@@ -30,7 +31,8 @@ int main(int argc, char** argv) {
   gStyle->SetNumberContours(255);
   gStyle->SetPalette(89);
 
-  bool DrawTraj = false;
+  bool DrawTraj = true;
+  bool DrawRecoHits = true;
 
   std::string filename = std::string(argv[1]);
 
@@ -64,7 +66,7 @@ int main(int argc, char** argv) {
   events->SetBranchAddress("Event", &event);
 
   int N_entries = events->GetEntries();
-  N_entries = 1000;
+  N_entries = 100;
   const double zmin = 3000;
   const double zmax = 19000;
   const double xmin = -4000;
@@ -72,51 +74,51 @@ int main(int argc, char** argv) {
 
   // Make some boxes for the fiducial volumes
   // Full view from inspecting all hits
-  TBox *xz_box_Full = new TBox(TMS_Const::TMS_Thin_Start,
-      -3485,
-      TMS_Const::TMS_Thick_End,
-      3485);
+  TBox *xz_box_Full = new TBox(TMS_Const::TMS_Thin_Start/1E3,
+      -3485/1E3,
+      TMS_Const::TMS_Thick_End/1E3,
+      3485/1E3);
   xz_box_Full->SetLineColor(kMagenta+2);
   xz_box_Full->SetFillStyle(0);
 
-  TBox *xz_box_LAr_Full = new TBox(TMS_Const::LAr_Start_Exact[2],
-      TMS_Const::LAr_Start_Exact[0],
-      TMS_Const::LAr_End_Exact[2],
-      TMS_Const::LAr_End_Exact[0]);
+  TBox *xz_box_LAr_Full = new TBox(TMS_Const::LAr_Start_Exact[2]/1E3,
+      TMS_Const::LAr_Start_Exact[0]/1E3,
+      TMS_Const::LAr_End_Exact[2]/1E3,
+      TMS_Const::LAr_End_Exact[0]/1E3);
   xz_box_LAr_Full->SetLineColor(kMagenta+2);
   xz_box_LAr_Full->SetFillStyle(0);
 
   // FV just taking 50 cm in from the full
   TBox *xz_box_FV = new TBox(xz_box_Full->GetX1(),
-      xz_box_Full->GetY1()+500,
-      xz_box_Full->GetX2()-500,
-      xz_box_Full->GetY2()-500);
+      (xz_box_Full->GetY1()+500/1E3),
+      (xz_box_Full->GetX2()-500/1E3),
+      (xz_box_Full->GetY2()-500/1E3));
   xz_box_FV->SetLineColor(kRed);
   xz_box_FV->SetLineStyle(kDashed);
   xz_box_FV->SetFillStyle(0);
 
-  TBox *xz_box_LAr_FV = new TBox(xz_box_LAr_Full->GetX1()+500,
-      xz_box_LAr_Full->GetY1()+500,
-      xz_box_LAr_Full->GetX2()-500,
-      xz_box_LAr_Full->GetY2()-500);
+  TBox *xz_box_LAr_FV = new TBox(xz_box_LAr_Full->GetX1()+500/1E3,
+      xz_box_LAr_Full->GetY1()+500/1E3,
+      xz_box_LAr_Full->GetX2()-500/1E3,
+      xz_box_LAr_Full->GetY2()-500/1E3);
   xz_box_LAr_FV->SetLineColor(kRed);
   xz_box_LAr_FV->SetLineStyle(kDashed);
   xz_box_LAr_FV->SetFillStyle(0);
 
 
   // Include the dead region boxes
-  TBox *xz_dead_top = new TBox(TMS_Const::TMS_Thin_Start,
-      TMS_Const::TMS_Dead_Top[0],
-      TMS_Const::TMS_Thick_End,
-      TMS_Const::TMS_Dead_Top[1]);
-  TBox *xz_dead_center = new TBox(TMS_Const::TMS_Thin_Start,
-      TMS_Const::TMS_Dead_Center[0],
-      TMS_Const::TMS_Thick_End,
-      TMS_Const::TMS_Dead_Center[0]);
-  TBox *xz_dead_bottom = new TBox(TMS_Const::TMS_Thin_Start,
-      TMS_Const::TMS_Dead_Bottom[0],
-      TMS_Const::TMS_Thick_End,
-      TMS_Const::TMS_Dead_Bottom[1]);
+  TBox *xz_dead_top = new TBox(TMS_Const::TMS_Thin_Start/1E3,
+      TMS_Const::TMS_Dead_Top[0]/1E3,
+      TMS_Const::TMS_Thick_End/1E3,
+      TMS_Const::TMS_Dead_Top[1]/1E3);
+  TBox *xz_dead_center = new TBox(TMS_Const::TMS_Thin_Start/1E3,
+      TMS_Const::TMS_Dead_Center[0]/1E3,
+      TMS_Const::TMS_Thick_End/1E3,
+      TMS_Const::TMS_Dead_Center[0]/1E3);
+  TBox *xz_dead_bottom = new TBox(TMS_Const::TMS_Thin_Start/1E3,
+      TMS_Const::TMS_Dead_Bottom[0]/1E3,
+      TMS_Const::TMS_Thick_End/1E3,
+      TMS_Const::TMS_Dead_Bottom[1]/1E3);
   xz_dead_top->SetFillStyle(3003);
   xz_dead_center->SetFillStyle(3003);
   xz_dead_bottom->SetFillStyle(3003);
@@ -125,13 +127,12 @@ int main(int argc, char** argv) {
   xz_dead_bottom->SetFillColor(kGray);
   
   // And a line at the thin/thick divide
-  TLine *xz_Thin_Thick = new TLine(TMS_Const::TMS_Thick_Start,
-      TMS_Const::TMS_Start_Exact[0],
-      TMS_Const::TMS_Thick_Start,
-      TMS_Const::TMS_End_Exact[0]);
+  TLine *xz_Thin_Thick = new TLine(TMS_Const::TMS_Thick_Start/1E3,
+      TMS_Const::TMS_Start_Exact[0]/1E3,
+      TMS_Const::TMS_Thick_Start/1E3,
+      TMS_Const::TMS_End_Exact[0]/1E3);
   xz_Thin_Thick->SetLineColor(kGray);
   xz_Thin_Thick->SetLineStyle(kDashed);
-
 
   TCanvas *canv = new TCanvas("canv", "canv", 1024, 1024);
   canv->SetLeftMargin(canv->GetLeftMargin()*1.4);
@@ -166,9 +167,10 @@ int main(int argc, char** argv) {
 
   gErrorIgnoreLevel = kWarning;
   int i = 0;
-  TH2D *plot = new TH2D("xz", "xz-view;z (mm); x (mm); Energy deposit (MeV)", 300, zmin, zmax, 200, xmin, xmax);
+  TH2D *plot = new TH2D("xz", "xz-view;z (m); x (m); Energy deposit (MeV)", 300, zmin/1E3, zmax/1E3, 200, xmin/1E3, xmax/1E3);
   plot->GetZaxis()->SetTitleOffset(plot->GetZaxis()->GetTitleOffset()*1.3);
 
+  TString zaxistitle = plot->GetZaxis()->GetTitle();
   for (; i < N_entries; ++i) {
     events->GetEntry(i);
     gRoo->GetEntry(i);
@@ -185,7 +187,7 @@ int main(int argc, char** argv) {
     double enu = tms_event.GetNeutrinoP4().E();
     TString reaction = tms_event.GetReaction().c_str();
     reaction.ReplaceAll(";",",");
-    plot->SetTitle(Form("#splitline{Event %i, #nu PDG: %i, E_{#nu}=%.2f GeV}{%s}", i, pdg, enu, reaction.Data()));
+    plot->SetTitle(Form("#splitline{Event %i, all true hits, #nu PDG: %i, E_{#nu}=%.2f GeV}{%s}", i, pdg, enu, reaction.Data()));
 
     std::vector<TMS_Hit> TMS_Hits = tms_event.GetHits();
     // First draw the hits
@@ -195,7 +197,7 @@ int main(int argc, char** argv) {
       double z = (*i).GetTrueHit().GetZ();
       double t = (*i).GetTrueHit().GetT();
       double e = (*i).GetTrueHit().GetE();
-      plot->Fill(z, x, e);
+      plot->Fill(z/1E3, x/1E3, e);
       pos[0] = x;
       pos[1] = y;
       pos[2] = z;
@@ -211,11 +213,12 @@ int main(int argc, char** argv) {
     std::vector<TGraph*> trajgraphs(ntraj);
     // Loop over the trajectories
     int it = 0;
+    int muonindex = 0;
     for (auto i = traj.begin(); i != traj.end(); ++i,it++) {
       TGraph *tempgraph = new TGraph((*i).GetPositionPoints().size());
       int npoints = int(((*i).GetPositionPoints()).size());
       for (int j = 0; j < npoints; ++j) {
-        tempgraph->SetPoint(j, (*i).GetPositionPoints()[j].Z(), (*i).GetPositionPoints()[j].X());
+        tempgraph->SetPoint(j, (*i).GetPositionPoints()[j].Z()/1E3, (*i).GetPositionPoints()[j].X()/1E3);
       }
 
       // Set a specific marker from primary particles
@@ -228,6 +231,7 @@ int main(int argc, char** argv) {
 
       if (abs((*i).GetPDG()) == 13) {
         tempgraph->SetMarkerColor(kYellow-3);
+        muonindex = it;
       } else if (abs((*i).GetPDG()) == 11) {
         tempgraph->SetMarkerColor(kGreen-2);
       } else if (abs((*i).GetPDG()) == 211) {
@@ -254,6 +258,7 @@ int main(int argc, char** argv) {
     plot->SetMinimum(0);
     plot->SetMaximum(2);
 
+    plot->GetZaxis()->SetTitle(zaxistitle);
     plot->Draw("colz");
     xz_box_FV->Draw("same");
     xz_box_LAr_FV->Draw("same");
@@ -264,18 +269,68 @@ int main(int argc, char** argv) {
     xz_dead_bottom->Draw("same");
     xz_Thin_Thick->Draw("same");
 
+    canv->Print(Outputname);
+
     // Go from back to front to have muon draw on top
     if (DrawTraj) {
       for (int i = trajgraphs.size()-1; i >= 0; --i) {
         if (trajgraphs[i] != NULL) trajgraphs[i]->Draw("P, same");
       }
+      // Draw muons on top
+      trajgraphs[muonindex]->Draw("P,same");
+      TString plottitle = plot->GetTitle();
+      plottitle.ReplaceAll("all true hits", "all true hits and traj.");
+      plot->SetTitle(plottitle);
+      canv->Print(Outputname);
     }
 
-    canv->Print(Outputname);
+    if (DrawRecoHits) {
+      // Clean up hits
+      std::vector<TMS_Hit> TMS_Hits_Cleaned = TMS_TrackFinder::GetFinder().CleanHits(TMS_Hits);
+      plot->Reset();
+      // First draw the hits
+      for (auto i = TMS_Hits_Cleaned.begin(); i != TMS_Hits_Cleaned.end(); ++i) {
+        double x = (*i).GetNotZ();
+        double z = (*i).GetZ();
+        plot->Fill(z/1E3, x/1E3);
+      }
+      plot->Draw("colz");
+      xz_box_FV->Draw("same");
+      xz_box_LAr_FV->Draw("same");
+      xz_box_Full->Draw("same");
+      xz_box_LAr_Full->Draw("same");
+      xz_dead_top->Draw("same");
+      xz_dead_center->Draw("same");
+      xz_dead_bottom->Draw("same");
+      xz_Thin_Thick->Draw("same");
+
+      TString plottitle = plot->GetTitle();
+      plottitle.ReplaceAll("all true hits and traj.", "all binary reco hits");
+      plot->SetTitle(plottitle);
+      // Also change the z-axis
+      plot->GetZaxis()->SetTitle("Binary hit");
+      plot->SetMaximum(1);
+
+      canv->Print(Outputname);
+
+      if (DrawTraj) {
+      for (int i = trajgraphs.size()-1; i >= 0; --i) {
+        if (trajgraphs[i] != NULL) trajgraphs[i]->Draw("P, same");
+      }
+      // Draw muons on top
+      trajgraphs[muonindex]->Draw("P,same");
+      TString plottitle = plot->GetTitle();
+      plottitle.ReplaceAll("all binary reco hits", "all binary reco hits and true traj.");
+      plot->SetTitle(plottitle);
+      canv->Print(Outputname);
+      }
+    }
 
     for (int i = 0; i < int(trajgraphs.size()); ++i) {
       delete trajgraphs[i];
     }
+    outfile->cd();
+    canv->Write(Form("Event_%i", i));
   }
 
   outfile->cd();
@@ -283,6 +338,8 @@ int main(int argc, char** argv) {
   outfile->Close();
 
   input->Close();
+  std::cout << "Wrote pdf file to " << Outputname << std::endl;
+  std::cout << "Wrote root file to " << Outputname_root << std::endl;
 
   canv->Print(Outputname+"]");
 }
