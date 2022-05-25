c85986894d1ba9e4e690b13d3b8d3cf24afd922b
Clarence Wret <cwret@fnal.gov>
2022-05-25 10:33:36 -0500

    Change cherry picker to draw events without reco, and showing the full LAr TMS view

--- app/CherryPickEvents.cpp
+++ app/CherryPickEvents.cpp
@@ -1,43 +1,58 @@
 // python code was very slow...
 #include <iostream>
+
 #include "TFile.h"
 #include "TTree.h"
 #include "TGeoManager.h"
 #include "TStopwatch.h"
+#include "TLorentzVector.h"
+#include "TH2D.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TGeoManager.h"
+#include "TStyle.h"
+#include "TPaveText.h"
+#include "TBox.h"
 
 // EDepSim includes
 #include "EDepSim/TG4Event.h"
-#include "EDepSim/TG4PrimaryVertex.h"
 
-// TMS includes
-// Geometry singleton
-#include "TMS_Geom.h"
-// Event class
 #include "TMS_Event.h"
-// Event viewer singleton
-#include "TMS_EventViewer.h"
-// Reconstructor
-#include "TMS_Reco.h"
-// TTree writer
-#include "TMS_TreeWriter.h"
-// General manager
-#include "TMS_Manager.h"
+#include "TMS_Constants.h"
+#include "TMS_Reco.h" // Needed for hit cleaning
 
-bool ConvertToTMSTree(std::string filename, std::string output_filename) {
-  std::cout << "Got " << filename << ", writing to " << output_filename << std::endl;
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cout << "Need one argument, the input filename" << std::endl;
+    return -1;
+  }
+
+  gStyle->SetOptStat(0);
+  gStyle->SetNumberContours(255);
+  gStyle->SetPalette(89);
+
+  bool DrawTraj = true;
+  //bool DrawRecoHits = true;
+  bool DrawRecoHits = false;
+
+  std::string filename = std::string(argv[1]);
+
+  std::cout << "Got " << filename << std::endl;
 
   // The input file
   TFile *input = new TFile(filename.c_str(), "open");
   // The EDepSim events
-  TTree *events = (TTree*)(input->Get("EDepSimEvents")->Clone("events"));
-  // The generator pass-through information
-  TTree *gRoo = (TTree*)(input->Get("DetSimPassThru/gRooTracker")->Clone("gRoo"));
+  TTree *events = (TTree*)input->Get("EDepSimEvents");
   // Get the detector geometry
   TGeoManager *geom = (TGeoManager*)input->Get("EDepSimGeometry");
 
-  // Get the event
-  TG4Event *event = NULL;
-  events->SetBranchAddress("Event", &event);
+  // The global manager
+  TMS_Manager::GetInstance().SetFileName(filename);
+
+  // Load up the geometry
+  TMS_Geom::GetInstance().SetGeometry(geom);
+  // The generator pass-through information
+  TTree *gRoo = (TTree*)input->Get("DetSimPassThru/gRooTracker");
   // Get the true neutrino vector from the gRooTracker object
   int StdHepPdg[__EDEP_SIM_MAX_PART__];
   double StdHepP4[__EDEP_SIM_MAX_PART__][4];
@@ -47,53 +62,146 @@ bool ConvertToTMSTree(std::string filename, std::string output_filename) {
   gRoo->SetBranchAddress("StdHepPdg", StdHepPdg);
   gRoo->SetBranchAddress("StdHepP4", StdHepP4);
 
-  // The global manager
-  TMS_Manager::GetInstance().SetFileName(filename);
-
-  // Load up the geometry
-  TMS_Geom::GetInstance().SetGeometry(geom);
+  // Get the event
+  TG4Event *event = NULL;
+  events->SetBranchAddress("Event", &event);
 
   int N_entries = events->GetEntries();
+  //N_entries = 500;
+  const double zmin = 3000;
+  const double zmax = 19000;
+  const double xmin = -4000;
+  const double xmax = 4000;
 
-  bool DrawPDF = TMS_Manager::GetInstance().Get_DrawPDF();
+  // Make some boxes for the fiducial volumes
+  // Full view from inspecting all hits
+  TBox *xz_box_Full = new TBox(TMS_Const::TMS_Thin_Start/1E3,
+      -3485/1E3,
+      TMS_Const::TMS_Thick_End/1E3,
+      3485/1E3);
+  xz_box_Full->SetLineColor(kMagenta+2);
+  xz_box_Full->SetFillStyle(0);
 
-  std::cout << "Starting loop over " << N_entries << " entries..." << std::endl;
-  TStopwatch Timer;
-  Timer.Start();
+  TBox *xz_box_LAr_Full = new TBox(TMS_Const::LAr_Start_Exact[2]/1E3,
+      TMS_Const::LAr_Start_Exact[0]/1E3,
+      TMS_Const::LAr_End_Exact[2]/1E3,
+      TMS_Const::LAr_End_Exact[0]/1E3);
+  xz_box_LAr_Full->SetLineColor(kMagenta+2);
+  xz_box_LAr_Full->SetFillStyle(0);
 
+  // FV just taking 50 cm in from the full
+  TBox *xz_box_FV = new TBox(xz_box_Full->GetX1(),
+      (xz_box_Full->GetY1()+500/1E3),
+      (xz_box_Full->GetX2()-500/1E3),
+      (xz_box_Full->GetY2()-500/1E3));
+  xz_box_FV->SetLineColor(kRed);
+  xz_box_FV->SetLineStyle(kDashed);
+  xz_box_FV->SetFillStyle(0);
+
+  TBox *xz_box_LAr_FV = new TBox(xz_box_LAr_Full->GetX1()+500/1E3,
+      xz_box_LAr_Full->GetY1()+500/1E3,
+      xz_box_LAr_Full->GetX2()-500/1E3,
+      xz_box_LAr_Full->GetY2()-500/1E3);
+  xz_box_LAr_FV->SetLineColor(kRed);
+  xz_box_LAr_FV->SetLineStyle(kDashed);
+  xz_box_LAr_FV->SetFillStyle(0);
+
+
+  // Include the dead region boxes
+  TBox *xz_dead_top = new TBox(TMS_Const::TMS_Thin_Start/1E3,
+      TMS_Const::TMS_Dead_Top[0]/1E3,
+      TMS_Const::TMS_Thick_End/1E3,
+      TMS_Const::TMS_Dead_Top[1]/1E3);
+  TBox *xz_dead_center = new TBox(TMS_Const::TMS_Thin_Start/1E3,
+      TMS_Const::TMS_Dead_Center[0]/1E3,
+      TMS_Const::TMS_Thick_End/1E3,
+      TMS_Const::TMS_Dead_Center[0]/1E3);
+  TBox *xz_dead_bottom = new TBox(TMS_Const::TMS_Thin_Start/1E3,
+      TMS_Const::TMS_Dead_Bottom[0]/1E3,
+      TMS_Const::TMS_Thick_End/1E3,
+      TMS_Const::TMS_Dead_Bottom[1]/1E3);
+  xz_dead_top->SetFillStyle(3003);
+  xz_dead_center->SetFillStyle(3003);
+  xz_dead_bottom->SetFillStyle(3003);
+  xz_dead_top->SetFillColor(kGray);
+  xz_dead_center->SetFillColor(kGray);
+  xz_dead_bottom->SetFillColor(kGray);
+  
+  // And a line at the thin/thick divide
+  TLine *xz_Thin_Thick = new TLine(TMS_Const::TMS_Thick_Start/1E3,
+      TMS_Const::TMS_Start_Exact[0]/1E3,
+      TMS_Const::TMS_Thick_Start/1E3,
+      TMS_Const::TMS_End_Exact[0]/1E3);
+  xz_Thin_Thick->SetLineColor(kGray);
+  xz_Thin_Thick->SetLineStyle(kDashed);
+
+  TCanvas *canv = new TCanvas("canv", "canv", 1024, 1024);
+  canv->SetLeftMargin(canv->GetLeftMargin()*1.4);
+  canv->SetRightMargin(canv->GetRightMargin()*1.5);
+  canv->SetTopMargin(canv->GetTopMargin()*1.2);
+  // Make the output name
+  std::string outputname = filename;
+  while (outputname.find("/") != std::string::npos) {
+    outputname = outputname.substr(outputname.find("/")+1, outputname.size());
+  }
+  TString Outputname = outputname.c_str();
+  Outputname.ReplaceAll(".root", "_Deposits.pdf");
+  canv->Print(Outputname+"[");
+
+  TString Outputname_root = outputname.c_str();
+  Outputname_root.ReplaceAll(".root", "_Deposits.root");
+  TFile *outfile = new TFile(Outputname_root, "recreate");
+  TTree *outtree = new TTree("energy_dep", "energy_dep");
+  float pos[4];
+  float energy;
+  outtree->Branch("Position", pos, "Position[4]/F");
+  outtree->Branch("Energy", &energy, "Energy/F");
+
+  TPaveText *text = new TPaveText(0,0,1,1,"NDC");
+  text->AddText(Form("Exec: %s", argv[0]));
+  text->AddText(Form("Input: %s", filename.c_str()));
+  text->AddText(Form("Output: %s", Outputname.Data()));
+  text->SetBorderSize(0);
+  text->SetFillColor(0);
+  text->Draw();
+  canv->Print(Outputname);
+
+  gErrorIgnoreLevel = kWarning;
   int i = 0;
-  N_entries = 1000;
-
-  // Do we overlay events
-  bool Overlay = false;
-  // How many events do we want to overlay?
-  int nOverlays = 5;
-  // The vector carrying our events that we want to overlay
-  std::vector<TMS_Event> overlay_events;
-  int npassed = 0;
+  TH2D *plot = new TH2D("xz", "xz-view;z (m); x (m); Energy deposit (MeV)", 400, zmin/1E3, zmax/1E3, 400, xmin/1E3, xmax/1E3);
+  plot->GetZaxis()->SetTitleOffset(plot->GetZaxis()->GetTitleOffset()*1.3);
+  TString zaxistitle = plot->GetZaxis()->GetTitle();
 
   for (; i < N_entries; ++i) {
     events->GetEntry(i);
     gRoo->GetEntry(i);
 
-#ifndef DEBUG
+    plot->Reset();
     if (i % (N_entries/10) == 0) {
-#endif
       std::cout << "Processed " << i << "/" << N_entries << " (" << double(i)*100./N_entries << "%)" << std::endl;
     }
 
-    // Make a TMS event
-    TMS_Event tms_event = TMS_Event(*event);
-    // Fill up truth information from the GRooTracker object
+    TMS_Event tms_event = TMS_Event(*event, true);
     tms_event.FillTruthFromGRooTracker(StdHepPdg, StdHepP4);
 
+    int pdg = tms_event.GetNeutrinoPDG();
+    double enu = tms_event.GetNeutrinoP4().E();
+    TString reaction = tms_event.GetReaction().c_str();
+    // Get the true muon info
+    double muonke = tms_event.GetMuonTrueKE();
+    double Emu = muonke + 106.5;
+    Emu *= 1E-3;
+
+    // Check if we actually want this event
     // Check that all points are EM as an example of cherry picking
     auto trueparts = tms_event.GetTrueParticles();
     bool Accept = true;
+    bool FoundMuon = false;
     for (auto &part: trueparts) {
       // Only look at muons
-      if (part.GetPDG() != 13) continue;
+      if (abs(part.GetPDG()) != 13) continue;
       if (!Accept) break;
+      FoundMuon = true;
 
       auto procs = part.GetProcessPoints();
       auto subprocs = part.GetSubprocessPoints();
@@ -105,83 +213,176 @@ bool ConvertToTMSTree(std::string filename, std::string output_filename) {
         if (proc != 2) {
           Accept = false;
           break;
-        } else {
+        }/* else {
           if (subprocs[num] != 22) {
             Accept = false;
             break;
           }
         }
+        */
         num++;
       }
     }
 
     // If we shouldn't continue
     if (!Accept) continue;
+    if (!FoundMuon) continue;
 
-    // Keep filling up the vector and move on to the next event
-    if (Overlay && i % nOverlays != 0) {
-      overlay_events.push_back(tms_event);
-      continue;
+    reaction.ReplaceAll(";",",");
+    plot->SetTitle(Form("#splitline{Event %i, all true hits, #nu PDG: %i, E_{#nu}=%.2f GeV, E_{#mu}=%.2f GeV}{%s}", i, pdg, enu, Emu, reaction.Data()));
+
+
+    std::vector<TMS_Hit> TMS_Hits = tms_event.GetHits();
+    // First draw the hits
+    for (auto i = TMS_Hits.begin(); i != TMS_Hits.end(); ++i) {
+      double x = (*i).GetTrueHit().GetX();
+      double y = (*i).GetTrueHit().GetY();
+      double z = (*i).GetTrueHit().GetZ();
+      double t = (*i).GetTrueHit().GetT();
+      double e = (*i).GetTrueHit().GetE();
+      plot->Fill(z/1E3, x/1E3, e);
+      pos[0] = x;
+      pos[1] = y;
+      pos[2] = z;
+      pos[3] = t;
+      energy = e;
+      outtree->Fill();
     }
 
-    // Add event information and truth from another event
-    if (Overlay && i % nOverlays == 0) {
-      // Now loop over previous events
-      for (auto &event : overlay_events) tms_event.AddEvent(event);
-      overlay_events.clear();
+    // Get the true particle's trajectories
+    std::vector<TMS_TrueParticle> traj = tms_event.GetTrueParticles();
+    int ntraj = traj.size();
+    // Make a TGraph for each trajectory
+    std::vector<TGraph*> trajgraphs(ntraj);
+    // Loop over the trajectories
+    int it = 0;
+    int muonindex = 0;
+    for (auto i = traj.begin(); i != traj.end(); ++i,it++) {
+      TGraph *tempgraph = new TGraph((*i).GetPositionPoints().size());
+      int npoints = int(((*i).GetPositionPoints()).size());
+      for (int j = 0; j < npoints; ++j) {
+        tempgraph->SetPoint(j, (*i).GetPositionPoints()[j].Z()/1E3, (*i).GetPositionPoints()[j].X()/1E3);
+      }
+
+      // Set a specific marker from primary particles
+      tempgraph->SetMarkerStyle(24);
+      tempgraph->SetMarkerSize(1.0);
+      if ((*i).GetParent() == -1) {
+        tempgraph->SetMarkerStyle(25);
+        tempgraph->SetMarkerSize(1.0);
+      } 
+
+      if (abs((*i).GetPDG()) == 13) {
+        tempgraph->SetMarkerColor(kYellow-3);
+        muonindex = it;
+      } else if (abs((*i).GetPDG()) == 11) {
+        tempgraph->SetMarkerColor(kGreen-2);
+      } else if (abs((*i).GetPDG()) == 211) {
+        tempgraph->SetMarkerColor(kRed-7);
+      } else if (abs((*i).GetPDG()) == 2212) {
+        tempgraph->SetMarkerColor(kMagenta-7);
+      } else if (abs((*i).GetPDG()) == 2112) {
+        tempgraph->SetMarkerColor(kCyan-3);
+        tempgraph->SetMarkerSize(0.0);
+        tempgraph->SetMarkerStyle(26);
+      } else if (abs((*i).GetPDG()) == 22) {
+        tempgraph->SetMarkerColor(kGray);
+        tempgraph->SetMarkerSize(0.0);
+        tempgraph->SetMarkerStyle(27);
+      }
+
+      trajgraphs[it] = tempgraph;
     }
 
-    // Dump information
-    //tms_event.Print();
+    // Now make the TGraphs for the true trajectory points
 
-    // Try finding some tracks
-    //TMS_TrackFinder::GetFinder().FindTracks(tms_event);
+    if (plot->Integral() == 0) continue;
 
-#ifdef DUNEANAOBJ_ENABLED
-    caf::SRTMS srtms = TMS_Utils::ConvertEvent();
-#endif
+    plot->SetMinimum(0);
+    plot->SetMaximum(3);
 
-    // View it
-    if (DrawPDF) TMS_EventViewer::GetViewer().Draw(tms_event);
-    // Write it
-    TMS_TreeWriter::GetWriter().Fill(tms_event);
-    std::cout << npassed << std::endl;
-    
-    npassed++;
-  } // End loop over all the events
+    plot->GetZaxis()->SetTitle(zaxistitle);
+    plot->Draw("colz");
+    xz_box_FV->Draw("same");
+    xz_box_LAr_FV->Draw("same");
+    xz_box_Full->Draw("same");
+    xz_box_LAr_Full->Draw("same");
+    xz_dead_top->Draw("same");
+    xz_dead_center->Draw("same");
+    xz_dead_bottom->Draw("same");
+    xz_Thin_Thick->Draw("same");
 
-  Timer.Stop();
-  std::cout << "Event loop took " << Timer.RealTime() << "s for " << i << " entries (" << Timer.RealTime()/N_entries << " s/entries)" << std::endl;
-  std::cout << npassed << " events passed selection" << std::endl;
+    canv->Print(Outputname);
 
-  TMS_TreeWriter::GetWriter().Write();
+    // Go from back to front to have muon draw on top
+    if (DrawTraj) {
+      for (int i = trajgraphs.size()-1; i >= 0; --i) {
+        if (trajgraphs[i] != NULL) trajgraphs[i]->Draw("P, same");
+      }
+      // Draw muons on top
+      trajgraphs[muonindex]->Draw("P,same");
+      TString plottitle = plot->GetTitle();
+      plottitle.ReplaceAll("all true hits", "all true hits and traj.");
+      plot->SetTitle(plottitle);
+      canv->Print(Outputname);
+    }
 
-  delete events;
-  delete gRoo;
+    if (DrawRecoHits) {
+      // Clean up hits
+      std::vector<TMS_Hit> TMS_Hits_Cleaned = TMS_TrackFinder::GetFinder().CleanHits(TMS_Hits);
+      plot->Reset();
+      // First draw the hits
+      for (auto i = TMS_Hits_Cleaned.begin(); i != TMS_Hits_Cleaned.end(); ++i) {
+        double x = (*i).GetNotZ();
+        double z = (*i).GetZ();
+        plot->Fill(z/1E3, x/1E3);
+      }
+      plot->Draw("colz");
+      xz_box_FV->Draw("same");
+      xz_box_LAr_FV->Draw("same");
+      xz_box_Full->Draw("same");
+      xz_box_LAr_Full->Draw("same");
+      xz_dead_top->Draw("same");
+      xz_dead_center->Draw("same");
+      xz_dead_bottom->Draw("same");
+      xz_Thin_Thick->Draw("same");
+
+      TString plottitle = plot->GetTitle();
+      plottitle.ReplaceAll("all true hits and traj.", "all binary reco hits");
+      plot->SetTitle(plottitle);
+      // Also change the z-axis
+      plot->GetZaxis()->SetTitle("Binary hit");
+      plot->SetMaximum(1);
+
+      canv->Print(Outputname);
+
+      if (DrawTraj) {
+      for (int i = trajgraphs.size()-1; i >= 0; --i) {
+        if (trajgraphs[i] != NULL) trajgraphs[i]->Draw("P, same");
+      }
+      // Draw muons on top
+      trajgraphs[muonindex]->Draw("P,same");
+      TString plottitle = plot->GetTitle();
+      plottitle.ReplaceAll("all binary reco hits", "all binary reco hits and true traj.");
+      plot->SetTitle(plottitle);
+      canv->Print(Outputname);
+      }
+    }
+
+    for (int i = 0; i < int(trajgraphs.size()); ++i) {
+      delete trajgraphs[i];
+    }
+    outfile->cd();
+    canv->Write(Form("Event_%i", i));
+  }
+
+  outfile->cd();
+  outtree->Write();
+  outfile->Close();
+
   input->Close();
+  std::cout << "Wrote pdf file to " << Outputname << std::endl;
+  std::cout << "Wrote root file to " << Outputname_root << std::endl;
 
-  return true;
-}
-
-int main(int argc, char **argv) {
-  if (argc != 2 && argc != 3) {
-    std::cerr << "Need one or two arguments: [EDepSim output file] [Output filename]" << std::endl;
-    return -1;
-  }
-
-  std::string EDepSimFile = std::string(argv[1]);
-  std::string OutputFile;
-
-  // If two arguments are given
-  if (argc == 2) {
-    std::string filename = std::string(argv[1]);
-    OutputFile = filename.substr(0, filename.find(".root"));
-    OutputFile += "_output.root";
-  } else {
-    OutputFile = std::string(argv[2]);
-  }
-
-  bool ok = ConvertToTMSTree(EDepSimFile, OutputFile);
-  if (ok) return 0;
-  else return -1;
+  canv->Print(Outputname+"]");
 }
