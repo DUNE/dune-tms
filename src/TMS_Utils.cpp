7c765b859e9780cf5cf6bb4858b90af79b3ae763
Clarence Wret <cwret@fnal.gov>
2022-05-24 19:49:49 -0500

    Add duneanaobj converter into utils (moved from singleton that is now deleted)

---
+++ src/TMS_Utils.cpp
@@ -0,0 +1,67 @@
+#include "TMS_Utils.h"
+
+//caf::SRTMS TMS_CAF_converter::ConvertEvent(TMS_Event &event) {
+#ifdef DUNEANAOBJ_ENABLED
+caf::SRTMS ConvertEvent() {
+
+  std::vector<std::vector<TMS_Hit> > LineCandidates = TMS_TrackFinder::GetFinder().GetLineCandidates();
+
+  // CAF object for TMS
+  caf::SRTMS caf;
+  caf.ntracks = LineCandidates.size();
+  // Loop over tracks and convert them to caf::SRTrack format
+  std::vector<caf::SRTrack> tracks = caf.tracks;
+  tracks.reserve(caf.ntracks);
+
+  // Build up number of total hits
+  int TotalHits = TMS_TrackFinder::GetFinder().GetCleanedHits().size();
+
+  // Fill the number of tracks from the track finder
+  for (auto it = LineCandidates.begin(); it != LineCandidates.end(); ++it) {
+    // Get the track
+    auto track = *it;
+
+    // caf tracks are pretty simple right now; 
+    // only start point, end point, start, direction and end direction
+    TMS_Hit firsthit = track.front();
+    TMS_Hit lasthit = track.back();
+    // Convert the TMS_Hit hit information to SRVector3D
+    caf::SRVector3D firsthit_conv(firsthit.GetNotZ(), 200, firsthit.GetZ()); // Have no y info in the TMS
+    caf::SRVector3D lasthit_conv(lasthit.GetNotZ(), 200, lasthit.GetZ()); // Have no y info in the TMS
+
+    caf::SRTrack srtrack;
+    srtrack.start = firsthit_conv;
+    srtrack.end = lasthit_conv;
+    srtrack.TrackQuality = double((*it).size())/TotalHits;
+    caf.tracks.push_back(srtrack);
+  }
+
+  std::vector<std::pair<bool, TF1*> > HoughLines = TMS_TrackFinder::GetFinder().GetHoughLines();
+  int nit = 0;
+  for (auto it = HoughLines.begin(); it != HoughLines.end(); ++it, ++nit) {
+    //double intercept = (*it).second->GetParameter(0);
+    //double slope = (*it).second->GetParameter(1);
+
+    // Calculate the z and x vectors by evaling the TF1 in thin and thick target
+    double zlow = TMS_Const::TMS_Thin_Start;
+    double zhi = TMS_Const::TMS_Thick_Start;
+    double xlow = (*it).second->Eval(zlow);
+    double xhi = (*it).second->Eval(zhi);
+
+    double zlen = zhi-zlow;
+    double xlen = xhi-xlow;
+    double len = sqrt(xlen*xlen+zlen*zlen);
+    zlen = zlen/len;
+    xlen = xlen/len;
+
+    caf::SRVector3D dir(xlen, 0, zlen); // Make the converted direction unit vector
+    tracks[nit].dir = dir;
+
+    // Now do the track quality, track energy and track length
+    tracks[nit].TrackLength_gcm3 = TMS_TrackFinder::GetFinder().GetTrackLength()[nit];
+    tracks[nit].TrackEnergy = TMS_TrackFinder::GetFinder().GetTrackEnergy()[nit];
+  }
+
+  return caf;
+}
+#endif
--- src/TMS_Utils.h
+++ src/TMS_Utils.h
@@ -1,11 +1,31 @@
 #ifndef __TMS_UTILS_H__
 #define __TMS_UTILS_H__
 
+#include <iostream>
+
+// Include the general TMS_Event class
+#include "TMS_Event.h"
+// The reco class
+#include "TMS_Reco.h"
+
+// Can only run this if we have linkage to CAF format
+#ifdef DUNEANAOBJ_ENABLED
+// Include standard record format for TMS
+#include "duneanaobj/StandardRecord/SRTMS.h"
+#endif
+
 namespace TMS_Utils {
+
   // Get the sign of a number
   template <typename T> inline int sgn(T value) {
     return (T(0)<value - (value<T(0)));
   }
+
+// Can only run this if we have linkage to CAF format
+#ifdef DUNEANAOBJ_ENABLED
+  caf::SRTMS ConvertEvent();
+#endif
+
 }
 
 #endif
