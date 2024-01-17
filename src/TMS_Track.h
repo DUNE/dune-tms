1a1ac5da44cc0737ff32076a0cfef4ba3809857f
Liam O'Sullivan <liam.osullivan@uni-mainz.de>
2024-01-17 11:32:02 +0100

    Added basic TMS_Track class with 3D track attributes

--- src/TMS_Track.h
+++ src/TMS_Track.h
@@ -1,5 +1,32 @@
-// General track class containing
+#include "TMS_Hit.h"
+
+#ifndef _TMS_TRACK_H_SEEN_
+#define _TMS_TRACK_H_SEEN_
+
+// General 3D-Track class
 class TMS_Track {
 
+  public:
+    TMS_Track() //std::vector<TMS_Hit>& OneTrack, std::vector<TMS_Hit>& OtherTrack)
+    {
+      // TODO: Take first and last hits and do the maffs
+      0;
+    };
+    void Print();
 
-}
+    double Start[3];     // Start point in x,y,z
+    double End[3];       // End point in x,y,z
+    double Direction[3]; // Unit vector in track direction
+    double Length;
+    double EnergyDeposit;
+    double EnergyRange;
+    double Time;         // TODO: Fill this in a sensible way
+
+    double GetEnergyDeposit(){return EnergyDeposit;};
+    double GetEnergyRange(){return EnergyRange;};
+
+    std::vector<TMS_Hit*> Hits;
+};
+
+
+#endif
