85b821c15366b3628931d55f6d194a64367aa285
Liam O'Sullivan <liam.osullivan@uni-mainz.de>
2024-05-03 06:47:37 -0500

    Modified setup.sh and Makefiles to use spack packages instead of ups

--- setup.sh
+++ setup.sh
@@ -1,51 +1,20 @@
 #!/bin/bash
+# @Liam on Slack when this breaks
 
-# Example of setup needed for SSRI dependencies
-# Here we use our own version of edep-sim, but you should be able to use one set up by ups
+# Setup FNAL spack
+source /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh
+# Source Liam's spack environment that includes dune_spack repository and package binaries
+source /exp/dune/data/users/losulliv/spack/my_spack/setup-env.sh
 
-MY_SSRI_DIR=/dune/app/users/cwret/SSRI
+# Load up a load of schtuff with spack
+#  - All the good stuff is built with gcc 12.2.0 toolchain, load newer cmake
+#  - Load root, geant4, clhep, expatn and edep-sim built with above toolchain
+spack load gcc@12.2.0
+spack load cmake@3.27.7%gcc@12.2.0
+spack load root@6.28.06%gcc@12.2.0
+spack load geant4@10.6.1%gcc@12.2.0
+spack load clhep@2.4.6.4%gcc@12.2.0
+spack load expat@2.5.0%gcc@12.2.0
+spack load edep-sim@3.2%gcc@12.2.0
 
-# Copied from Gavin's SSRI setup
-source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-
-#setup dk2nu        v01_05_01b   -q e15:prof
-#setup genie        v2_12_10c    -q e15:prof
-#setup genie_xsec   v2_12_10     -q DefaultPlusValenciaMEC
-#setup genie_phyopt v2_12_10     -q dkcharmtau
-#setup geant4       v4_10_3_p01b -q e15:prof
-#setup ifdhc
-
-#setup root v6_22_08b -f Linux64bit+3.10-2.17 -q e20:p383b:prof
-setup cmake v3_17_3 -f Linux64bit+3.10-2.17
-
-# Official duneanaobj UPS products doesn't include TMS info yet...
-#setup duneanaobj v01_00_00 -f Linux64bit+3.10-2.17 -q debug:e17:gv2
-
-# Setup custom UPS products, for CAFana format development
-#source /dune/app/users/cwret/ups/setup.sh
-#setup duneanaobj v02_01_01 -q e20:gv3:prof
-# Flag for enabling linking against DUNEANAOBJ
-#if [ ! -z "${DUNEANAOBJ_INC}" ]; then
-  #export DUNEANAOBJ_ENABLED=1
-  #echo "Enabling DUNEANAOBJ..."
-#fi
-
-# Need CLHEP for unit conversion in edep-sim info
-setup clhep v2_4_4_1 -f Linux64bit+3.10-2.17 -q debug:e20
-# Specific requirements for duneanaobj
-setup root v6_12_06a -f Linux64bit+3.10-2.17 -q e15:prof
-
-# Set up edep-sim
-setup edepsim v3_2_0 -f Linux64bit+3.10-2.17 -q e20:prof
-
-# Add TMS execs and library directory to env
-export TMS_DIR=${PWD}
-export PATH=${PATH}:${TMS_DIR}/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TMS_DIR}/lib
-
-# Debugging options passed to makefile
-#export DEBUG=1
-#export VERBOSE=1
-
-
-echo "Setup TMS environment"
+echo "Setup TMS environment :)"
