8f215e184313a07564a0c8322a06c1fadf130ee8
Liam O'Sullivan <liam.osullivan@uni-mainz.de>
2024-05-31 10:39:57 -0500

    fixed root version

--- setup_FNAL.sh
+++ setup_FNAL.sh
@@ -13,7 +13,7 @@ source /exp/dune/data/users/losulliv/spack/my_spack/setup-env.sh
 #  - Load root, geant4, clhep, expatn and edep-sim built with above toolchain
 spack load gcc@12.2.0%gcc@11.4.1    && echo "Setup gcc..."
 spack load cmake@3.27.7%gcc@12.2.0  && echo "Setup cmake..."
-spack load root@6.28.12%gcc@12.2.0  && echo "Setup root..."
+spack load root@6.28.06%gcc@12.2.0  && echo "Setup root..."
 spack load geant4@10.6.1%gcc@12.2.0 && echo "Setup geant4..."
 spack load clhep@2.4.6.4%gcc@12.2.0 && echo "Setup clhep..."
 spack load expat@2.5.0%gcc@12.2.0   && echo "Setup expat..."
