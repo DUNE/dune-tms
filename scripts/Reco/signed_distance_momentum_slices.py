b6706697bb1a3b08d5abaae6441b2632551f8a09
Michael Dolce <mike.dolce8@gmail.com>
2024-08-07 16:03:13 -0400

    signed_distance_momentum_slices.py, was using the wrong index before

--- scripts/Reco/signed_distance_momentum_slices.py
+++ scripts/Reco/signed_distance_momentum_slices.py
@@ -55,17 +55,17 @@ def run(c, truth, outfilename, nmax=-1):
     # create histogram, and bin edges are from -2m -- 2m. This is a list of TH1s
     hist_signed_distance_muon = [ROOT.TH1D(f"muon_{i}", "", 100, -2000, 2000) for i in range(len(bin_edges)-1)]
     hist_signed_distance_amuon = [ROOT.TH1D(f"amuon_{i}", "", 100, -2000, 2000) for i in range(len(bin_edges)-1)]
-    
+
     # Set axis labels for each histogram
+    edge_counter = 0
     for hist in hist_signed_distance_muon + hist_signed_distance_amuon:
-        i = 0
         hist.SetXTitle("Signed Distance (mm)")
         hist.SetYTitle("Events")
         if hist.GetName().startswith("muon"):  # only want to set the title for the first histogram drawn
-            hist.SetTitle("{lo} < KE_{mu} < {hi} GeV".format(lo=bin_edges_gev[i], hi=bin_edges_gev[i+1], mu='#mu'))
-            i += 1
+            hist.SetTitle(f"{bin_edges_gev[edge_counter]} < KE\_{'#mu'} < {bin_edges_gev[edge_counter + 1]} GeV")
         elif hist.GetName().startswith("amuon"):
             hist.SetTitle("")  # Remove the histogram title
+        edge_counter += 1
 
     nevents = min(c.GetEntries(), nmax if nmax >= 0 else float('inf'))
     print_every = max(1, nevents // 10)
