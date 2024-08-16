2c606882cb839ee4c7b6072563dea9774023875e
Michael Dolce <mike.dolce8@gmail.com>
2024-08-16 15:39:46 -0400

    signed_distance_enu_momentum_slices.py, we have Enu that are very large -- over 5 GeV. skip over them. and address some typos

--- scripts/Reco/signed_distance_enu_momentum_slices.py
+++ scripts/Reco/signed_distance_enu_momentum_slices.py
@@ -24,26 +24,12 @@ FUDICIAL_CUT = 50
 LAR_START = (-3478.48, -2166.71, 4179.24)
 LAR_END = (3478.48, 829.282, 9135.88)
 
-class Momentum:
-    def __init__(self, kinetic_energy, classification="muon"):
-        self.ke = kinetic_energy
-        self.classification = classification
-        self.ranges = [(0, 250), (250, 500), (500, 750), (750, 1000),
-                       (1000, 2000), (2000, 3000), (3000, 4000),
-                       (4000, 4250), (4250, 4500), (4500, 4750), (4750, 5000)]  # MeV
-
-    def get_muon_ke_index(self):
-        for i, r in enumerate(self.ranges):
-            if r[0] <= self.ke <= r[1]:
-                return i
-        return None
-
 
 class Neutrino:
     def __init__(self, energy, classification='nu'):
         self.energy = energy
         self.classification = classification
-        self.ranges = [(i, i + 500) for i in range(0, 5000, 500)]  # in 500 MeV slices
+        self.ranges = [(i, i + 500) for i in range(0, 5001, 500)]  # in 500 MeV slices
 
     def get_enu_index(self):
         for i, r in enumerate(self.ranges):
@@ -75,7 +61,7 @@ def region3(x):
     return 2500 < x < 4000
 
 def run(c, truth, outfilename, nmax=-1):
-    bin_edges = array.array('d', [i for i in range(0, 5000, 500)])  # in 500 MeV slices
+    bin_edges = array.array('d', [i for i in range(0, 5001, 500)])  # in 500 MeV slices
 
     # create histogram, and bin edges are from -2m -- 2m. This is a list of TH1s
     # create histograms that will be sliced based on the Neutrino Energy
@@ -141,7 +127,10 @@ def run(c, truth, outfilename, nmax=-1):
                     e_nu = Neutrino(enu * 1000, classification="nu" if pdg == 13 else "nubar")
                     range_index_nu = e_nu.get_enu_index()
                     if range_index_nu is not None and signed_dist is not None:
-                        if pdg == 14:
+                        if e_nu.energy > 5000:  # skip if the energy is too high
+                            print('Ev = {} MeV. Too high and skipping.'.format(e_nu.energy))
+                            continue
+                        if pdg == 13:
                             hist_signed_distance_enu_lar[range_index_nu].Fill(signed_dist)
                         else:
                             hist_signed_distance_enubar_lar[range_index_nu].Fill(signed_dist)
@@ -163,7 +152,7 @@ def run(c, truth, outfilename, nmax=-1):
 
     edge_counter = 0
     # loop through the mu and amu hists together for LAr KE and TMS KE.
-    for index, (hists_mu_list, hists_amu_list) in enumerate([hist_signed_distance_enu_lar, hist_signed_distance_enubar_lar]):
+    for index, (hists_mu_list, hists_amu_list) in enumerate([[hist_signed_distance_enu_lar, hist_signed_distance_enubar_lar]]):
         for hist_mu, hist_amu in zip(hists_mu_list, hists_amu_list):  # pair these together
             hist_mu.SetLineColor(ROOT.kRed)
             hist_amu.SetLineColor(ROOT.kBlue)
