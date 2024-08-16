035329f70b990ce7c83efc2c8f53b15355538d14
Michael Dolce <mike.dolce8@gmail.com>
2024-08-16 16:01:48 -0400

    signed_distance_enu_momentum_slices.py, we have Enu that are very large -- over 5 GeV. If we get those, skip over them.

--- scripts/Reco/signed_distance_enu_momentum_slices.py
+++ scripts/Reco/signed_distance_enu_momentum_slices.py
@@ -127,6 +127,9 @@ def run(c, truth, outfilename, nmax=-1):
                     e_nu = Neutrino(enu * 1000, classification="nu" if pdg == 13 else "nubar")
                     range_index_nu = e_nu.get_enu_index()
                     if range_index_nu is not None and signed_dist is not None:
+                        if range_index_nu > len(e_nu.ranges) - 1:  # skip if the energy is too high
+                            print('Ev = {} MeV. Too high and skipping.', e_nu.energy)
+                            continue
                         print('range_index_nu', range_index_nu, '. enu, ', e_nu.energy)
                         if pdg == 13:
                             hist_signed_distance_enu_lar[range_index_nu].Fill(signed_dist)
