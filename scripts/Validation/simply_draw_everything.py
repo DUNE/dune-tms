0e0ffb6cfd774c12332dd64696a9e20bdd405c20
Jeffrey Kleykamp <jeffrey.kleykamp@gmail.com>
2024-09-24 21:00:15 -0500

    Add ability to do reco eff plots automatically

--- scripts/Validation/simply_draw_everything.py
+++ scripts/Validation/simply_draw_everything.py
@@ -14,6 +14,9 @@ def draw_histograms(input_file):
     os.makedirs(output_dir, exist_ok=True)
     
     canvas = ROOT.TCanvas("canvas", "canvas", 800, 600)
+    
+    recoeff_plots_numerators = dict()
+    recoeff_plots_denominators = dict()
 
     # Loop over all keys in the ROOT file
     for key in root_file.GetListOfKeys():
@@ -23,6 +26,10 @@ def draw_histograms(input_file):
         split = name.split("_")
         if len(split) > 2:
             output_subdir = os.path.join(output_dir, split[0])
+        reco_eff = False
+        if "numerator" in name or "denominator" in name: reco_eff = True
+        if reco_eff:
+            output_subdir = os.path.join(output_subdir, "additional_plots")
         os.makedirs(output_subdir, exist_ok=True)  
         if isinstance(obj, ROOT.TH2):
             # For 2D histograms, draw with "colz" option and save as png
@@ -38,6 +45,41 @@ def draw_histograms(input_file):
             obj.Draw()
             #print(f"{obj.GetName()} integral: {obj.Integral()}")
             canvas.Print(os.path.join(output_subdir, obj.GetName() + ".png"))
+        if reco_eff:
+            key = name.replace("_numerator", "").replace("_denominator", "")
+            if "numerator" in name: recoeff_plots_numerators[key] = obj 
+            if "denominator" in name: recoeff_plots_denominators[key] = obj 
+            
+    all_names = set(recoeff_plots_numerators.keys()) & set(recoeff_plots_denominators.keys())
+    for name in all_names:
+        error = False
+        if name not in recoeff_plots_numerators:
+            print(f"Didn't find {name} in {recoeff_plots_numerators}")
+            error = True
+        if name not in recoeff_plots_denominators:
+            print(f"Didn't find {name} in {recoeff_plots_denominators}")
+            error = True
+        if error:
+            print(f"Had one or more errors, skipping {name}")
+            continue
+        numerator = recoeff_plots_numerators[name]
+        denominator = recoeff_plots_denominators[name]
+        newtitle = numerator.GetTitle()
+        newtitle = newtitle.replace("Numerator", "").strip()
+        numerator.SetTitle(newtitle)
+        numerator.Divide(denominator)
+        numerator.GetYaxis().SetRangeUser(0, 1.2)
+        numerator.GetYaxis().SetTitle("Reconstruction Efficiency")
+        numerator.Draw()
+        
+        
+        output_subdir = output_dir
+        name = numerator.GetName()
+        name = name.replace("_numerator", "")
+        split = name.split("_")
+        if len(split) > 1:
+            output_subdir = os.path.join(output_dir, split[0])
+        canvas.Print(os.path.join(output_subdir, name + ".png"))
 
     # Close the input ROOT file
     root_file.Close()
