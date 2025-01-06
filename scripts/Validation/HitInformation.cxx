145c47d78b5dd2438778a045f0815f142f12a280
Jeffrey Kleykamp <jeffrey.kleykamp@gmail.com>
2025-01-06 13:43:55 -0600

    Add hit to label

--- scripts/Validation/HitInformation.cxx
+++ scripts/Validation/HitInformation.cxx
@@ -1,19 +1,29 @@
 {
   // Reco loses PE relative to true, so need different bounds
-  REGISTER_AXIS(pe_reco, std::make_tuple("PE_{Reco}", 50, 0, 100)); 
-  REGISTER_AXIS(pe_true, std::make_tuple("PE_{True}", 50, 0, 200));
+  REGISTER_AXIS(pe_reco, std::make_tuple("Hit PE_{Reco}", 50, 0, 100)); 
+  REGISTER_AXIS(pe_true, std::make_tuple("Hit PE_{True}", 50, 0, 200));
   
-  REGISTER_AXIS(plane, std::make_tuple("Plane", 61, 0, 60)); // Goes up to 50 for new geom
-  REGISTER_AXIS(bar, std::make_tuple("Bar", 251, 0, 250)); // Goes up to ~200, but leave room for "dune simulation" tag in 2d
+  REGISTER_AXIS(e_reco, std::make_tuple("Hit E_{Reco} (MeV)", 50, 0, 10)); 
+  REGISTER_AXIS(e_true, std::make_tuple("Hit E_{True} (MeV)", 50, 0, 10));
+  
+  REGISTER_AXIS(plane, std::make_tuple("Hit Plane", 61, 0, 60)); // Goes up to 50 for new geom
+  REGISTER_AXIS(bar, std::make_tuple("Hit Bar", 251, 0, 250)); // Goes up to ~200, but leave room for "dune simulation" tag in 2d
   
   for (int hi = 0; hi < truth.NTrueHits; hi++) {
     if (!truth.TrueRecoHitIsPedSupped[hi]) {
       GetHist("basic__hit_information__pe_reco", 
-                      "Reco PE", "pe_reco")->Fill(truth.TrueRecoHitPE[hi]);
+                      "Reco Hit PE", "pe_reco")->Fill(truth.TrueRecoHitPE[hi]);
       GetHist("basic__hit_information__pe_true", 
-                      "True PE", "pe_true")->Fill(truth.TrueHitPE[hi]);
-      GetHist("basic__hit_information__pe_comparison", "True PE", "pe_true",
+                      "True Hit PE", "pe_true")->Fill(truth.TrueHitPE[hi]);
+      GetHist("basic__hit_information__pe_comparison", "Reco vs True Hit PE", "pe_true",
               "pe_reco")->Fill(truth.TrueHitPE[hi], truth.TrueRecoHitPE[hi]);
+              
+      GetHist("basic__hit_information__e_reco", 
+                      "Reco Hit E", "e_reco")->Fill(truth.TrueRecoHitE[hi]);
+      GetHist("basic__hit_information__e_true", 
+                      "True Hit E", "e_true")->Fill(truth.TrueHitE[hi]);
+      GetHist("basic__hit_information__e_comparison", "Reco vs True Hit E", "e_true",
+              "e_reco")->Fill(truth.TrueHitE[hi], truth.TrueRecoHitE[hi]);
               
       GetHist("basic__hit_information__occupancy_plane", "Hit Plane Number",
               "plane")->Fill(truth.TrueHitPlane[hi]);
