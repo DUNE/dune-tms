c6a66a3cd1005e048324a2dd4ff0589b947c6f36
Jeffrey Kleykamp <jeffrey.kleykamp@gmail.com>
2025-01-20 17:11:18 -0600

    Add hit delta t

--- scripts/Validation/HitInformation.cxx
+++ scripts/Validation/HitInformation.cxx
@@ -9,6 +9,9 @@
   REGISTER_AXIS(plane, std::make_tuple("Hit Plane", 61, 0, 60)); // Goes up to 50 for new geom
   REGISTER_AXIS(bar, std::make_tuple("Hit Bar", 251, 0, 250)); // Goes up to ~200, but leave room for "dune simulation" tag in 2d
   
+  
+  REGISTER_AXIS(hit_delta_t, std::make_tuple("Hit #Delta T (ns, reco - true)", 50, -100, 100));
+  
   for (int hi = 0; hi < truth.NTrueHits; hi++) {
     if (!truth.TrueRecoHitIsPedSupped[hi]) {
       GetHist("basic__hit_information__pe_reco", 
@@ -31,6 +34,21 @@
               "bar")->Fill(truth.TrueHitBar[hi]);
       GetHist("basic__hit_information__occupancy_plane_bar", "Hit Plane vs Bar Number",
               "plane", "bar")->Fill(truth.TrueHitPlane[hi], truth.TrueHitBar[hi]);
+              
+      GetHist("basic__hit_information__delta_t", "Hit #Delta T (Reco - True), All Hits",
+              "hit_delta_t")->Fill(truth.TrueRecoHitT[hi] - truth.TrueHitT[hi]);
+      if (truth.TrueHitView[hi] == 0)
+          GetHist("basic__hit_information__delta_t_xbar_only", "Hit #Delta T (Reco - True), X Hits Only",
+                  "hit_delta_t")->Fill(truth.TrueRecoHitT[hi] - truth.TrueHitT[hi]);
+      if (truth.TrueHitView[hi] == 1)
+          GetHist("basic__hit_information__delta_t_ybar_only", "Hit #Delta T (Reco - True), Y Hits Only",
+                  "hit_delta_t")->Fill(truth.TrueRecoHitT[hi] - truth.TrueHitT[hi]);
+      if (truth.TrueHitView[hi] == 2)
+          GetHist("basic__hit_information__delta_t_ubar_only", "Hit #Delta T (Reco - True), U Hits Only",
+                  "hit_delta_t")->Fill(truth.TrueRecoHitT[hi] - truth.TrueHitT[hi]);
+      if (truth.TrueHitView[hi] == 3)
+          GetHist("basic__hit_information__delta_t_vbar_only", "Hit #Delta T (Reco - True), V Hits Only",
+                  "hit_delta_t")->Fill(truth.TrueRecoHitT[hi] - truth.TrueHitT[hi]);
               
     } // end if (!truth.TrueRecoHitIsPedSupped[hi])
   } // end for (int hi = 0; hi < truth.NTrueHits; hi++)
