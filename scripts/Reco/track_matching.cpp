96baa138561bf1cc14e658088b52db80e2bd7b3c
Katherine Hildebrandt <khildebr@dunegpvm03.fnal.gov>
2025-04-02 11:27:20 -0500

    Added booleans to check TMS containment and PDG (isMuon)

--- scripts/Reco/track_matching.cpp
+++ scripts/Reco/track_matching.cpp
@@ -7,10 +7,41 @@ using namespace std;
 
 float ZGap = 2250; //the distance between ND-LAr and TMS in mm
 
+bool isTMSContained(float xpos, float ypos, float zpos){
+	bool out = true;
+	
+	if(xpos > 3300 || xpos < -3300){
+		out = false;
+	}
+	if(ypos > 160 || ypos < -2850){
+		out = false;
+	}
+	if(zpos > 18314 || zpos < 11362){ //tms end is 18314mm for new thick/thin geometry; is 13500mm for thin planes only
+		out = false;
+	}
+
+	return out;
+}
+
+bool isMuon(int PDG){
+	bool out = false;
+
+	if(std::abs(PDG) == 13){
+		out = true;
+	}
+
+	return out;
+}
+
 int main(std::string filepath){
 
 	std::unique_ptr<TFile> myFile(TFile::Open(filepath.c_str()));
 	
+	if(!myFile || myFile->IsZombie()){
+		std::cerr << "Error opening file" << endl;
+		exit(-1);
+	}
+
 	TTree *truth = myFile->Get<TTree>("Truth_Info");
 	TTree *reco = myFile->Get<TTree>("Reco_Tree");
 
