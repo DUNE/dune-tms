6ea7e573ce2d11b57baecb7beb3874961aed324f
Jeffrey Kleykamp <jeffrey.kleykamp@gmail.com>
2025-04-08 20:03:11 -0500

    fix up plots

--- scripts/Validation/TimeSlicing.cxx
+++ scripts/Validation/TimeSlicing.cxx
@@ -4,17 +4,25 @@
 #define bound_within_1(x) (x > 0.999) ? 0.999 : x
   REGISTER_AXIS(
       slice_completeness,
-      std::make_tuple(
-          "Slice Completeness (Primary Int Vis E, in Slice / in Spill)", 20,
-          0.0, 1.0));
-  REGISTER_AXIS(slice_cleanliness,
-                std::make_tuple(
-                    "Slice Cleanliness  (Slice Vis E, Primary Int / All Ints)",
-                    20, 0.0, 1.0));
-  GetHist("time_slicing__slice_completeness", "Slice Cleanliness",
+      std::make_tuple("Primary Int. E_{Vis}, Slice / Spill", 20, 0.0, 1.0));
+  REGISTER_AXIS(
+      slice_cleanliness,
+      std::make_tuple("Slice E_{Vis}, Primary / All Ints.", 20, 0.0, 1.0));
+  REGISTER_AXIS(slice_cleanliness_times_completeness,
+                std::make_tuple("Cleanliness x Completeness", 20, 0.0, 1.0));
+  GetHist("time_slicing__slice_completeness",
+          "Slice Cleanliness, ie. How much of E_{Vis} in Slice is from Primary "
+          "Int?",
           "slice_cleanliness")
       ->Fill(bound_within_1(truth.PrimaryVertexVisibleEnergyFraction));
-  GetHist("time_slicing__slice_cleanliness", "Slice Completeness",
-          "slice_completeness")
+  GetHist(
+      "time_slicing__slice_cleanliness",
+      "Slice Completeness, ie. How much of Primary Int.'s E_{Vis} is in Slice?",
+      "slice_completeness")
       ->Fill(bound_within_1(truth.VertexVisibleEnergyFractionInSlice));
+  GetHist("time_slicing__slice_cleanliness_times_completeness",
+          "Slice Completeness Times Completeness",
+          "slice_cleanliness_times_completeness")
+      ->Fill(bound_within_1(truth.VertexVisibleEnergyFractionInSlice *
+                            truth.PrimaryVertexVisibleEnergyFraction));
 }
