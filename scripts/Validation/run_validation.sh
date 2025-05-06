8a1f4de13e30b22c911730846b25781014b7f33a
Jeffrey Kleykamp <jeffrey.kleykamp@gmail.com>
2025-05-05 21:20:11 -0500

    Allow for input to be subdir

--- scripts/Validation/run_validation.sh
+++ scripts/Validation/run_validation.sh
@@ -1,7 +1,7 @@
 infile="$1"
 shift
 base_filename=$(basename "$infile")
-outfile="/exp/dune/data/users/${USER}/dune-tms/Validation/Tracking_Validation/${base_filename}"
+outfile="/exp/dune/data/users/${USER}/dune-tms/Validation/Tracking_Validation/${base_filename%.root}.root"
 make && ./Tracking_Validation "$infile" $@ && python simply_draw_everything.py "${outfile}"
 echo "Output should be in"
 echo "${outfile/.root/_images}"
